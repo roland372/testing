4b8d651995303fb3944a4e65f9797d220f23d301
roland372 <damfat94@gmail.com>
2022-08-18 00:44:22 +0200

    test

--- backend/app.js
+++ backend/app.js
@@ -5,4 +5,5 @@
 // require    - function to use modules (CommonJS)
 // module     - info about current module (file)
 // process    - info about env where the program is being executed
+
 // console.log(__dirname)
